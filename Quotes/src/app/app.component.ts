7c196f380b15c0b129d00edd155b69ef3541fd2c
john victor <johnvictor0002@gmail.com>
2019-03-24 16:58:04 +0300

    added initial details to the project

---
+++ Quotes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  Quotes = 'Everyone thinks of changing the world,but no one thinks of changing himself.'
+}
