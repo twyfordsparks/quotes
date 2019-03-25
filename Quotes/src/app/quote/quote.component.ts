400bc96c6f03875a02cef3c45b02d861b096703a
john victor <johnvictor0002@gmail.com>
2019-03-25 12:50:23 +0300

    added time functionality to the project

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -8,13 +8,13 @@ import {Quote}  from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'Its not lack of love,but a lack of friendship that makes unhappy marriages','Happiness is a direction, not a place.'),
-    new Quote(2, 'We accept the love we think we desreve', 'Happiness is a direction, not a place.'),
-    new Quote(3, 'Dont cry because it is over,smile because it happened', 'Happiness is a direction, not a place.'),
-    new Quote(4, 'For every minute you are angry,you lose sixty seconds of happines', 'Happiness is a direction, not a place.'),
-    new Quote(5, 'The good you do today will be forgotten tomorrow', 'Happiness is a direction, not a place.'),
-    new Quote(6, 'A friend is someone who knows all about you and still loves you', 'Happiness is a direction, not a place.'),
-    new Quote(7, 'Love is like the wind, you cant see it but you can feel it', 'Happiness is a direction, not a place.')
+    new Quote(1, 'Its not lack of love,but a lack of friendship that makes unhappy marriages','Happiness is a direction, not a place.',new Date(25,4,2019)),
+    new Quote(2, 'We accept the love we think we desreve', 'Happiness is a direction, not a place.', new Date(25, 4, 2019)),
+    new Quote(3, 'Dont cry because it is over,smile because it happened', 'Happiness is a direction, not a place.', new Date(25, 4, 2019)),
+    new Quote(4, 'For every minute you are angry,you lose sixty seconds of happines', 'Happiness is a direction, not a place.', new Date(25, 4, 2019)),
+    new Quote(5, 'The good you do today will be forgotten tomorrow', 'Happiness is a direction, not a place.', new Date(25, 4, 2019)),
+    new Quote(6, 'A friend is someone who knows all about you and still loves you', 'Happiness is a direction, not a place.', new Date(25, 4, 2019)),
+    new Quote(7, 'Love is like the wind, you cant see it but you can feel it', 'Happiness is a direction, not a place.', new Date(25, 4, 2019))
   ]
   completeQuote(isComplete, index) {
     if (isComplete) {
