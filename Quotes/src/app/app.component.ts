0aa83cf6075c589ad9755ca1912911089dfd2a85
john victor <johnvictor0002@gmail.com>
2019-03-25 09:57:41 +0300

    added the string to the app component

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string[]
-  constructor(){
-    this.quotes = ['It s not lack of love,but a lack of friendship that makes unhappy marriages.','We accept the love we think we deserve.','Dont cry because it is over,smile because it happened.','For every minute you are angry,you lose sixty seconds of happines.','The good you do today will be forgotten tomorrow.','A friend is someone who knows all about you and still loves you.','Love is like the wind,you cant see it but you can feel it.']
-  }
+  quotes = [
+    new Quote(1,'Its not lack of love,but a lock of friendship that makes unhappy marriages.'),
+    new Quote(2, 'We accept the love we think we desreve.'),
+    new Quote(3, 'Dont cry because it is over,smile because it happened.'),
+    new Quote(4, 'For every minute you are angry,you lose sixty seconds of happines.'),
+    new Quote(5, 'The good you do today will be forgotten tomorrow.'),
+    new Quote(6, 'A friend is someone who knows all about you and still loves you.'),
+    new Quote(7, 'Love is like the wind, you cant see it but you can feel it.'),
+  ]
 }
